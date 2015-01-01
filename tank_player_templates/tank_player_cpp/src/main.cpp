3afb644c9618d48afd60f166af938ea0e8b97fa3
Björn Weström <bjowes@gmail.com>
2015-01-01 20:36:40 +0100

    Corrected player name UTF-8 printing

--- tank_player_templates/tank_player_cpp/src/main.cpp
+++ tank_player_templates/tank_player_cpp/src/main.cpp
@@ -14,21 +14,13 @@ int main(int argc, const char** argv)
     setlocale(LC_ALL, "en_US.UTF-8");
 
     auto playerName=TankLogic::PlayerName;
-    std::cout << "Stargin player with name.. " << argv[1] << std::endl;
-    std::wcout << "Starting player with name " << playerName << std::endl;
-
-    int len = 0;
-    for (const char *c = argv[1]; *c; c++, len++) {
-        printf("%x ", (*c));
-        fflush(stdout);
-    }
 
     if (argc>1)
     {
         playerName=Safir::Dob::Typesystem::Utilities::ToWstring(argv[1]);
     }
 
-    std::wcout << "Starting player with name " << playerName << std::endl;
+    std::cout << "Starting player with name " << Safir::Dob::Typesystem::Utilities::ToUtf8(playerName) << std::endl;
 
     //start running the program
     boost::asio::io_service ioService;
