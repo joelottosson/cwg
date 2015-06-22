20dd58aa6ec0235c66b0db7948ba48961c69d5fd
thegrandmother <henrik.sommerland@gmail.com>
2015-06-22 15:19:39 +0200

    Moved constructors to speedup compajling

--- src/tank_game_gui/src/gamemodel.h
+++ src/tank_game_gui/src/gamemodel.h
@@ -142,43 +142,11 @@ struct Dude
     mutable int current_frame = 0;
     mutable qint64 last_update;
 
-    Dude(QPointF position, Direction direction):
-    	position(position),
-		moveDirection(direction),
-    	visible(true),
-		dying(false),
-    	paintPosition(position)
-    {
-    	walking_sprite.image=QPixmap(":/images/tux-anim.png");
-		walking_sprite.lifeTime=500;
-		for (int i=0; i < 4; ++i)
-		{
-			walking_sprite.fragments.push_back(QRectF(i*72, 0, 72, 72));
-		}
+    Dude(QPointF position, Direction direction);
 
-		walking_frames = 4;
+    void updateFramecounter(SpriteData sprite ) const;
 
-		dead_sprite.image=QPixmap(":/images/animated-dead-tux.png");
-		dead_sprite.lifeTime=500;
-		for (int i=0; i < 4; ++i)
-		{
-			dead_sprite.fragments.push_back(QRectF(i*72, 0, 72, 72));
-		}
 
-		dead_frames = 4;
-
-		current_frame = 0;
-		last_update = 0;
-
-    }
-
-    void updateFramecounter(SpriteData sprite ) const {
-    	qint64 elapsed_time = QDateTime::currentMSecsSinceEpoch() - last_update;
-    	if(elapsed_time >= (qint64)(sprite.lifeTime/sprite.fragments.size())){
-    		current_frame = (current_frame + 1) % sprite.fragments.size();
-    		last_update = QDateTime::currentMSecsSinceEpoch();
-    	}
-    }
 
 };
 typedef std::vector<Dude> DudeMap;
