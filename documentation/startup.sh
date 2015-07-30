4b06244e01e628c7783cc0b05bc2db530838e1e1
Björn Weström <bjowes@gmail.com>
2015-07-30 18:00:58 +0200

    Fixed for Mate window manager

--- documentation/startup.sh
+++ documentation/startup.sh
@@ -10,10 +10,11 @@ pkill tank_engine
 pkill dope_main
 pkill dose_main
 
-gnome-terminal --show-menubar --geometry=120x100 \
-  --tab --title "dose" -e "bash -c 'dose_main'" \
+mate-terminal --show-menubar --geometry=120x100 \
+  --window --title "dose" -e "bash -c 'dose_main'" \
   --tab-with-profile=Default --title "dope" -e "bash -c 'dope_main'" \
-  --tab-with-profile=Default --title "tank_engine" -e "bash -c 'tank_engine'" \
+  --tab-with-profile=Default --title "tank_engine" -e "bash -c 'sleep 3 && tank_engine'" \
+  --tab-with-profile=Default --title "tank_match_server" -e "bash -c 'sleep 3 && tank_match_server'" \
   --tab-with-profile=Default --title "tank_game_gui" \
   --tab-with-profile=Default --title "Player1" \
   --tab-with-profile=Default --title "Player2" \
